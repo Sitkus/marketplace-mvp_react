8c0adde78f44f5e4c040b65442855cf33a2a796d
Lukas Sitkus <luk@box4metal.com>
2022-08-23 23:09:14 +0300

    Change dynamic route prop

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,5 +1,18 @@
 function CartPage() {
-    return (<div />);
+    const cartItems = [
+        'Hello',
+        'Again'
+    ]
+
+    return (
+          <div>
+            Are you ready to purchase these?
+
+            <ul>
+              {cartItems.map((cartItem) => <li key={cartItem}>{cartItem}</li>)}
+            </ul>
+          </div>
+    );
 }
 
 export default CartPage;
