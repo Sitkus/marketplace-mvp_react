b2d7eb8c62ba1da812e607d8604cbcfcd0a8e1c2
Lukas Sitkus <luk@box4metal.com>
2022-08-23 22:41:28 +0300

    Add react-router; Add base routes structure

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '~/components/HomePage.js';
+import ProductPage from '~/components/ProductPage.js';
+import CartPage from '~/components/CartPage.js';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+
+      <Route exact path="/cart" element={<CartPage />} />
+
+      <Route exact path="/products/:id" element={<ProductPage />} />
+
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
