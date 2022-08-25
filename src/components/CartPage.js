38ee2dd45a5a26cec43f3f9ca6f1263656c30d30
Lukas Sitkus <luk@box4metal.com>
2022-08-25 10:19:44 +0300

    Add remove from cart functionality

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,14 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem } from '../store';
 
 function CartPage() {
+    const dispatch = useDispatch();
     const cartItems = useSelector(state => state.cart);
+
+    function removeCartItem(index) {
+        dispatch(removeItem({ index }));
+    }
 
     return cartItems.length ? (
         <div>
             Are you ready to purchase these?
             <ul>
                 {cartItems.map((cartItem, index) => (
-                    <li key={`${cartItem.name}-${index}`}>{cartItem.name}</li>
+                    <li key={`${cartItem.name}-${index}`}>
+                        {cartItem.name}
+                        <button onClick={() => removeCartItem(index)}>Delete item</button>
+                    </li>
                 ))}
             </ul>
         </div>
